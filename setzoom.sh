d963c4fbaa58271e3dd1e7b0e77b090516b06be3
Marcelo <mvrozanti@hotmail.com>
2018-06-09 13:52:10 -0300

    choosable zoom

--- setzoom.sh
+++ setzoom.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-DEFAULT_STEAM_CLIENT_PATH='/home/nexor/.steam/steam/steamapps/common/dota 2 beta/game/dota/bin/linuxsteamrt64/libclient.so'
+DEFAULT_STEAM_CLIENT_PATH='~/.steam/steam/steamapps/common/dota\ 2\ beta/game/dota/bin/linuxsteamrt64/libclient.so'
 if [ -f "$DIRECTORY" ]; then echo Located default client path; else echo "Could not find default dota 2 client path"; exit 1; fi
 if [ $(grep -c "$DEFAULT_STEAM_CLIENT_PATH") -eq 0 ]; then
     read -p "New zoom: " $zoom
     case $zoom in
         ''|*[!0-9]*) echo "Bad zoom entered" ;;
-        *) sed -i 's/1134/1200/' "$DEFAULT_STEAM_CLIENT_PATH" ;;
+        *) sed -i 's/1134/$zoom/' "$DEFAULT_STEAM_CLIENT_PATH" ;;
     esac
 else
     echo "Zoom was already set"
